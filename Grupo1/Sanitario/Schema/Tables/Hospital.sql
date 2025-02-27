cae8c865e6ec308d2a1a0b642cbae3f58327b847
Marcelo Claure <mclaure@gmail.com>
2025-02-26 20:56:02 -0400

    Adding backup file

--- Grupo1/Sanitario/Schema/Tables/Hospital.sql
+++ Grupo1/Sanitario/Schema/Tables/Hospital.sql
@@ -4,5 +4,6 @@
     nombre VARCHAR(255) NOT NULL,
     ciudad VARCHAR(255) NOT NULL,
     telefono VARCHAR(20) NOT NULL,
+    codigo_postal VARCHAR(20) NULL,
     director INT UNIQUE NULL -- Se relaciona con Médico
 );
