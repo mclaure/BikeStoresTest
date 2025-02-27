309a0e2a9dacba55f9e4d1630ed1fcfb84340c16
Marcelo Claure <mclaure@gmail.com>
2025-02-26 20:16:20 -0400

    Initial version

---
+++ Grupo1/Sanitario/Schema/Tables/Hospital.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Hospital 
+(
+    codHospital INT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    ciudad VARCHAR(255) NOT NULL,
+    telefono VARCHAR(20) NOT NULL,
+    director INT UNIQUE NULL -- Se relaciona con Médico
+);
